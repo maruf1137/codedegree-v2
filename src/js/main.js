6cf9d96d95541adc28df8beb5792378e57689fde
Maruf Mia <mmaruf1137@gmail.com>
2022-06-05 21:43:23 +0600

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,74 +1,18 @@
-const bars = document.querySelector(".bars");
-const closeSidebar = document.querySelector(".close-sidebar");
-const sidebar = document.querySelector(".sidebar");
+const bars = document.querySelector('.mbl-bar');
+const closeSidebar = document.querySelector('.close-mbl-sidebar');
+const sidebar = document.querySelector('.mbl-sidebar');
 
-bars.addEventListener("click", () => {
-  console.log("sidebar");
-  sidebar.classList.toggle("show-sidebar");
+bars.addEventListener('click', () => {
+	sidebar.classList.add('show-mbl-sidebar');
 });
-closeSidebar.addEventListener("click", () => {
-  console.log("sidebar");
-  sidebar.classList.remove("show-sidebar");
+closeSidebar.addEventListener('click', () => {
+	sidebar.classList.remove('show-mbl-sidebar');
 });
 
-// custom cursor
+// dropdown
+const dropdown = document.querySelector('.dropdown');
+const dropbtn = document.querySelector('.dropdown__btn');
 
-const cursor = document.querySelector(".cursor");
-
-gsap.to(cursor, { xPercent: -50, yPercent: -50 });
-
-window.addEventListener("mousemove", (e) => {
-  // console.log(e.x);
-  gsap.to(cursor, { x: e.x, y: e.y });
-});
-
-const cursorShow = () => {
-  cursor.style.display = "flex";
-};
-
-const cursorHide = () => {
-  cursor.style.display = "none";
-};
-
-const project = document.querySelector(".project");
-project.addEventListener("mouseover", cursorShow);
-project.addEventListener("mouseout", cursorHide);
-
-// swiper js
-
-var swiper = new Swiper(".projectSwiper", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    575: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-  },
-});
-
-var swiper = new Swiper(".platformSwiper", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
-var swiper = new Swiper(".articleSwiper", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
-
-const swiperBtn = document.querySelectorAll(".swiper-pagination-bullet");
-swiperBtn.forEach((e) => {
-  e.textContent = "";
+dropbtn.addEventListener('click', function () {
+	dropdown.classList.toggle('show-dropdown');
 });
